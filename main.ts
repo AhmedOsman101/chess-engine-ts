cb7479698e7b46fb47d4a2f0b17509c4e6dca370
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-07 00:31:12 +0200

    build: add oak framework and update deno config
    - Added @oak/oak dependency
    - Updated deno.json with imports and tasks
    - Generated deno.lock file
    - Implemented basic oak server in main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-if (import.meta.main) {
-  //
-}
+import { Application } from "jsr:@oak/oak/application";
+import { Router } from "jsr:@oak/oak/router";
+
+const PORT = 5000;
+
+const router = new Router();
+
+router.get("/", ctx => {
+  ctx.response.body = "hello world!";
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: PORT });
+
+console.log(`server is running at: http://0.0.0.0:${PORT}`);
