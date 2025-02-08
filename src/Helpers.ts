ed3ba2e4b8125c5e1a9725587bd01c94f149e34e
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-08 22:28:10 +0200

    feat(Helpers): add printBoard function
    - Added printBoard function to visualize board
    - Uses Unicode chess symbols for pieces
    - Improved isNumber function type handling
    - Added PieceChar import
    - Enhanced board representation clarity

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,3 +1,5 @@
+import { PieceChar } from "./board/Piece.ts";
+
 export const getUnique = (arr: number[]): number | undefined => {
   let zeroCount = 0;
   const unique = arr.reduce((a, b) => {
@@ -17,7 +19,7 @@ export const getRandomInt = (min: number, max: number): number => {
 export const getChar = (index: number): string => String.fromCharCode(index);
 export const char2num = (char: string): number => char.charCodeAt(0);
 
-export const isNumber = (str: string): boolean => !Number.isNaN(+str);
+export const isNumber = (value: string | number) => !Number.isNaN(+value);
 
 export function* range(
   start = 0,
@@ -29,4 +31,36 @@ export function* range(
     yield i;
     i += step;
   }
+}
+
+export function printBoard(arr: Array<number>): void {
+  const lightSquare = ""; // U+0020 SPACE
+  const darkSquare = "󰝤"; //   U+2588 FULL BLOCK
+  const squareColor = (i: number) => (i % 2 === 0 ? lightSquare : darkSquare);
+  const pieces = {
+    R: "♜",
+    N: "♞",
+    B: "♝",
+    Q: "♛",
+    K: "♚",
+    P: "♟︎", // Black pieces (Unicode chess symbols)
+    r: "♖",
+    n: "♘",
+    b: "♗",
+    q: "♕",
+    k: "♔",
+    p: "♙", // White pieces (Unicode chess symbols)
+  };
+
+  for (let i = 0; i < arr.length; i += 8) {
+    const row = 8 - i / 8;
+    const line = arr.slice(i, i + 8).map((cell, col) => {
+      const piece = PieceChar[cell];
+      // console.log(`${col}, ${row}`);
+      return piece ? pieces[piece] : squareColor(col + row);
+    });
+    console.log(`${row} |`, line.join(" "));
+  }
+  console.log("   -----------------");
+  console.log("    A B C D E F G H");
 }
